45a2e4f53a6c61c6736d641953c7f081636aea4b
mikelaslam <kipkorir385@gmail.com>
2022-11-29 17:15:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -24,20 +24,21 @@ if(location===50 || location===60)
 if(location<42)
     return 1584
 }
-function calculatesFarePrice(start, destination){
-    let distance= distanceTravelledInFeet(start, destination)
-    if(distance===43,44){
+function calculatesFarePrice(beginning, end){
+let distance= distanceTravelledInFeet(beginning, end)
+    if(beginning===43 && end===44){
         return 0;
     }
 
-    if(distance===34 && distance===32) {
-    return 2.56
+    if(beginning===34 && end===32) {
+    return (((beginning - end)*264)- 400)*0.02
+
     }
-    if(distance>=2000 && distance <=2500){
+    if(beginning===50 && end ===58){
     return 25;
     }
-    else if(distance>2500) {
-        return "cannot travel that far"
+    else {
+        return "cannot travel that far";
     }
 
 } 
